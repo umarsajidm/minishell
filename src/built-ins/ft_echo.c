b44a7df0040000b2f813ba696bc72bfdcef9044e
Joonatan Tarvainen <jtarvain@c1r6p7.hive.fi>
2025-11-08 17:22:01 +0200

    weird edgecase fixes for ft_echo -n -n -n-n works now

--- src/built-ins/ft_echo.c
+++ src/built-ins/ft_echo.c
@@ -1,29 +1,56 @@
-#include "minishell.h"
+//#include "minishell.h"
+#include <stdio.h>
+#include <string.h>
 
-int	ft_echo(char **argv)
+static int	is_flag(const char *s)
+{
+	int	i;
+
+	i = 2;
+	if (s[0] != '-')
+		return (0);
+	if (s[1] != 'n')
+		return (0);
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	if (!s[i])
+		return (1);
+	return (0);
+}
+
+int	ft_echo(char **av)
 {
 	int	i;
 	int	newline;
 
 	i = 1;
-	if (argv[1] && ft_strcmp("-n", argv[1]) == 0)
+	newline = 1;
+	while (is_flag(av[i]))
 	{
 		newline = 0;
-		i = 2;
+		i++;
 	}
-	else
-		newline = 1;
-	while (argv[i])
+	while (av[i])
 	{
-		if (printf("%s", argv[i]) < 0)
+		if (printf("%s", av[i]) < 0)
 			return (1);
 		i++;
-		if (argv[i])
+		if (av[i])
 			if (printf(" ") < 0)
 				return (1);
 	}
 	if (newline)
 		if (printf("\n") < 0)
 			return (1);
+	return (0);
+}
+
+int main(int ac, char **av)
+{
+	ft_echo(av);
 	return (0);
 }
