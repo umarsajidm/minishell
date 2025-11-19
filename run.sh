91ee109e7322d87efd0e146b7340bfbe891be1bc
root <root@MahnoorUmar.localdomain>
2025-11-19 23:28:38 +0200

    adding the scripts for running and deploying

--- run.sh
+++ run.sh
@@ -1,22 +1,41 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
-# Check if commit message is provided
-if [ -z "$1" ]; then
-  echo "Usage: ./run.sh \"commit message\" [branch-name]"
-  exit 1
+echo "PROGRAM = '$program_name'"
+make
+if [ $? -ne 0 ]; then  exit 1
 fi
 
-# Set default branch if not provided
-BRANCH=${2:-main}
+program_name=$(grep -E '^NAME' Makefile | awk -F'=' '{print $NF}' | awk '{print $NF}')
 
-# Pull latest changes
-git pull origin "$BRANCH"
+#without any flags 
 
-# Stage all changes
-git add .
+if [ -z "$1" ]; then
+	./"$program_name"
+	exit 0
+fi
 
-# Commit with provided message
-git commit -m "$1"
+#with classic valgrind
 
-# Push to the specified branch
-git push origin "$BRANCH"
+if [ "$1" == "l" ]; then
+	valgrind ./"$program_name"
+
+#with valgrind --leak-check=ful
+
+elif [ "$1" == "lf" ]; then
+	valgrind --leak-check=full ./"$program_name"
+ 
+#with valgrind file descriptors
+
+elif [ "$1" == "fd" ]; then
+	valgrind --track-fd=yes ./"$program_name"
+
+#with valgrind memleak and file descriptors
+
+elif [ "$1" == "fdl" ]; then
+	valgrind --leak-check=full --track-fds=yes ./"$program_name"
+
+
+else
+	echo "usage; ./run.sh [l | lf | fdl]"
+fi
+
