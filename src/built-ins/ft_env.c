79bb91cc1ebec99541db40f6a3d6499cf221f4cf
Umar <umarsajidm989@gmail.com>
2025-12-10 19:46:06 +0200

    wip

--- src/built-ins/ft_env.c
+++ src/built-ins/ft_env.c
@@ -1,26 +1,3 @@
-// #include "minishell.h"
-
-// int	ft_env(t_env *head)
-// {
-// 	while (head)
-// 	{
-// 		if (!head->value)
-// 		{
-// 			head = head->next;
-// 			continue;
-// 		}
-// 		ft_putstr_fd(head->key, STDOUT_FILENO);
-// 		if (head->value)
-// 		{
-// 			ft_putchar_fd('=', STDOUT_FILENO);
-// 			ft_putstr_fd(head->value, STDOUT_FILENO);
-// 		}
-// 		ft_putchar_fd('\n', STDOUT_FILENO);
-// 		head = head->next;
-// 	}
-// 	return (0);
-// }
-
 #include "minishell.h"
 
 int	ft_env(t_env *head)
@@ -32,10 +9,13 @@ int	ft_env(t_env *head)
 			head = head->next;
 			continue;
 		}
+		ft_putstr_fd(head->key, STDOUT_FILENO);
 		if (head->value)
-			printf("%s=%s\n", head->key, head->value);
-		else
-			printf("%s\n", head->key);
+		{
+			ft_putchar_fd('=', STDOUT_FILENO);
+			ft_putstr_fd(head->value, STDOUT_FILENO);
+		}
+		ft_putchar_fd('\n', STDOUT_FILENO);
 		head = head->next;
 	}
 	return (0);
