af2f0e1f302b15588d3f65676e2f45fe74380dd1
Umar <umarsajidm989@gmail.com>
2025-12-17 19:05:39 +0200

    norminetted pipeline

--- src/executor/pipeline.c
+++ src/executor/pipeline.c
@@ -1,62 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipeline.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: musajid <musajid@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 19:03:08 by musajid           #+#    #+#             */
+/*   Updated: 2025/12/17 19:03:08 by musajid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	waitstatus(pid_t pid, t_shell *shell);
-
-void	init_fd(t_fd	*fd)
-{
-	fd->fd[0] = -1;
-	fd->fd[1] = -1;
-	fd->prev_fd = -1;
-	fd->in_fd = -1;
-	fd->out_fd = -1;
-}
-
-void	close_fd(t_fd *fd)
-{
-	if (fd->fd[0] >= 0)
-		close(fd->fd[0]);
-	if (fd->fd[1] >= 0)
-		close(fd->fd[1]);
-	if (fd->prev_fd >= 0)
-		close(fd->prev_fd);
-	if (fd->in_fd >= 0)
-		close(fd->in_fd);
-	if (fd->out_fd >= 0)
-		close(fd->out_fd);
-	init_fd(fd);
-}
-
-void	parent_loop(t_cmd *cmd, t_fd *fd)
-{
-	if (fd->prev_fd != -1)
-		close(fd->prev_fd);
-	if (cmd->next != NULL)
-		fd->prev_fd = fd->fd[0];
-	else
-		fd->prev_fd = -1;
-	close(fd->fd[1]);
-}
-
-int	fds_manipulation_and_execution(t_cmd *command,
-	t_shell *shell, t_exec *exec)
-{
-	if (exec->fd->prev_fd != -1)
-		dup2(exec->fd->prev_fd, STDIN_FILENO);
-	if (exec->fd->in_fd != -1)
-		dup2(exec->fd->in_fd, STDIN_FILENO);
-	if (command->next != NULL && exec->fd->out_fd == -1)
-		dup2(exec->fd->fd[1], STDOUT_FILENO);
-	if (exec->fd->out_fd != -1)
-		dup2(exec->fd->out_fd, STDOUT_FILENO);
-	close_fd(exec->fd);
-	if (execution(command, shell, exec) == 1)
-		exit_after_execve(shell, exec);
-	free(exec->path_to_exec);
-	if (exec->envp != NULL)
-		freearray(exec->envp);
-	execution_cleanup(shell);
-	exit(shell->exit_code);
-}
 
 static int	setup_builtin_redirs(t_shell *shell, t_cmd *command,
 								int *saved_stdin, int *saved_stdout)
@@ -64,25 +20,22 @@ static int	setup_builtin_redirs(t_shell *shell, t_cmd *command,
 	if (!command->redirs)
 		return (0);
 	if (applying_redir(command, &shell->exec->fd->in_fd,
-				&shell->exec->fd->out_fd) == 1)
+			&shell->exec->fd->out_fd) == 1)
 		return (1);
-
 	if (shell->exec->fd->out_fd != -1)
 	{
 		*saved_stdout = dup(STDOUT_FILENO);
 		dup2(shell->exec->fd->out_fd, STDOUT_FILENO);
 	}
-
 	if (shell->exec->fd->in_fd != -1)
 	{
 		*saved_stdin = dup(STDIN_FILENO);
 		dup2(shell->exec->fd->in_fd, STDIN_FILENO);
 	}
-
 	return (0);
 }
 
-static void restore_builtin_fds(int saved_stdin, int saved_stdout,
+static void	estore_builtin_fds(int saved_stdin, int saved_stdout,
 								t_fd *fd)
 {
 	if (saved_stdout != -1)
@@ -92,18 +45,19 @@ static void restore_builtin_fds(int saved_stdin, int saved_stdout,
 	close_fd(fd);
 }
 
-static void handle_single_builtin(t_shell *shell, t_cmd *command)
+static void	handle_single_builtin(t_shell *shell, t_cmd *command)
 {
-	int saved_stdout = -1;
-	int saved_stdin = -1;
+	int	saved_stdout;
+	int	saved_stdin;
 
+	saved_stdout = -1;
+	saved_stdin = -1;
 	if (setup_builtin_redirs(shell, command,
 			&saved_stdin, &saved_stdout) == 1)
 	{
 		shell->exit_code = GENERAL_ERROR;
-		return;
+		return ;
 	}
-
 	shell->exit_code = run_builtin(command, shell, false);
 	restore_builtin_fds(saved_stdin, saved_stdout, shell->exec->fd);
 }
